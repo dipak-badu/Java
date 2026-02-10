747641518fc549c707df997baed56a1d2f1d63af
Dipak badu <dipakbadu123@gmail.com>
2026-02-10 15:50:54 +0545

    default and parameterized constructor

--- Constructor.java
+++ Constructor.java
@@ -10,6 +10,13 @@ class Rectangle{
 
     }
 
+    // default constructor
+
+    Rectangle(){
+      length =60;
+      breadth =50;  
+    }
+
     int calculateArea(){
         return length* breadth;
     }
@@ -17,10 +24,18 @@ class Rectangle{
 
 public class Constructor{
     public static void main(String[] args){
-        int area;
+        int area1 , area2;
         Rectangle obj = new Rectangle(20, 40);
-        area = obj.calculateArea();
+        area1 = obj.calculateArea(); // parameterized constructor
 
-        System.out.println("|The area of recatangle: "+area);
+        System.out.println("|The area of recatangle: "+area1);
+
+        Rectangle obj1= new Rectangle();
+        area2 = obj1.calculateArea(); // default constructor 
+        System.out.println("|The area of recatangle: "+area2);
+
+        
+
+        
     }
 }
