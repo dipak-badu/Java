254d5d338119e8e6576c319630698b848b86df7b
Dipak badu <dipakbadu123@gmail.com>
2026-02-12 21:41:11 +0545

    Static modifier

---
+++ StaticMOdifier.java
@@ -0,0 +1,33 @@
+// class StaticTest{
+//     //non static method 
+
+//     int multiply(int a, int b){
+//           return a*b;
+//     }
+
+//     // static method
+
+//     static int  add(int a, int b){
+//         return a+b;
+//     } 
+// }
+
+// public class StaticMOdifier {
+
+//     public static void main(String[] args){
+//         StaticTest text = new StaticTest();
+//         //call non static method
+//         System.out.println("multiply = "+ text.multiply(5, 8));
+
+//         // call static  method
+//         System.out.println("sum = "+StaticTest.add(5,7));
+//     }
+// }
+
+
+
+
+// Static Variable  we can asscess by using the class aame as like static method
+
+
+
